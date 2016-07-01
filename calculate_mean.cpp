c9beaed6117afa93201bbaa20d6e6e652fa5698c
unknown <>
2016-07-01 22:28:48 +0900

    add calculate_mean.cpp

---
+++ calculate_mean.cpp
@@ -0,0 +1,38 @@
+nclude <type_traits>
+
+template
+<
+typename InputItr,
+	 std::enable_if_t<std::is_arithmetic<typename std::decay_t<InputItr>::value_type>::value>* 
+	 = nullptr
+	 >
+	 constexpr inline auto sum(InputItr first, InputItr last) noexcept
+{
+	using ValType = std::decay_t<decltype(*first)>;
+	auto result{static_cast<ValType>(0)};
+	while(first != last){result += *(first++);}
+	return result;
+}
+
+#include <iterator>
+
+template
+<
+typename InputItr,
+	 std::enable_if_t<std::is_arithmetic<typename std::decay_t<InputItr>::value_type>::value>* 
+	 = nullptr
+	 >
+	 constexpr inline auto mean(InputItr first, InputItr last) noexcept
+{
+	return sum(first, last) / std::distance(first, last);
+}
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	//This test program calculates mean of data.
+	std::vector<double> data{1, 2, 3, 4, 5};
+	std::cout << mean(data.begin(), data.end()) << std::endl;
+}
